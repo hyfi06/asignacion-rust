e56e31e7ba197541eaa98a238ae9a6a5e84a2690
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-10-30 17:54:05 -0600

    snapshot 30oct24
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- src/genetic.rs
+++ src/genetic.rs
@@ -3,19 +3,11 @@ pub trait Individual {
     fn cross(&self, other: &Self) -> Self;
 }
 
+
 pub struct State {
     chromosome: Vec<bool>,
 }
 
-impl Individual for State {
-    fn mutate(&mut self) {
-        todo!()
-    }
-
-    fn cross(&self, other: &Self) -> Self {
-        todo!()
-    }
-}
 
 pub fn genetic(initial_population: &[State], generations: usize) {
   
